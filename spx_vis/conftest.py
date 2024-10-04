bb90539d843ea5ff54086aa5657ec11329c026ab
Connor Howington <connor.howington@gmail.com>
2024-10-04 18:11:37 -0400

    Initial commit

---
+++ spx_vis/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from spx_vis.users.models import User
+from spx_vis.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
