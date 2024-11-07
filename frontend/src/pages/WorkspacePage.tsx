a78d14b53fde8a7514437354825fb91d7b0f95f1
Connor Howington <connor.howington@gmail.com>
2024-11-07 17:32:41 -0500

    Disable upload button when logged out

--- frontend/src/pages/WorkspacePage.tsx
+++ frontend/src/pages/WorkspacePage.tsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from 'react';
-import Button from 'react-bootstrap/Button';
+import Button from '../components/Button';
 
 import DatasetTable from '../components/DatasetTable';
 import FileUploadModal from '../components/FileUploadModal';
 import { getDatasets, SigMFFilePairResponse } from '../apiClient/fileService';
+import { useAppContext } from '../utils/AppContext';
 
 const WorkspacePage = () => {
+  const context = useAppContext();
   const [showModal, setShowModal] = useState(false);
   const [datasets, setDatasets] = useState<SigMFFilePairResponse>([]);
 
@@ -23,7 +25,12 @@ const WorkspacePage = () => {
       <DatasetTable datasets={datasets} />
       <br />
       <h5>Add a New Dataset</h5>
-      <Button variant="primary" onClick={() => setShowModal(true)}>
+      <Button
+        variant="primary"
+        onClick={() => setShowModal(true)}
+        disabled={!context?.username}
+        disabledHelpText="You must be logged in to upload a dataset"
+      >
         Upload Dataset
       </Button>
       <FileUploadModal
