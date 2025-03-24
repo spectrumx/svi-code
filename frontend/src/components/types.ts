626cd79b5b9348ab7b026ce94b68d6fb3f17f373
Connor Howington <connor.howington@gmail.com>
2025-03-24 13:34:03 -0400

    Add comment

--- frontend/src/components/types.ts
+++ frontend/src/components/types.ts
@@ -10,6 +10,13 @@ export type FilesWithContent = {
   [key: string]: FileWithContent;
 };
 
+/**
+ * Props for Viz Container components (e.g. WaterfallVizContainer,
+ * SpectrogramVizContainer, etc.) All Viz Container components should have
+ * these exact props, whereas Viz components (e.g. WaterfallVisualization,
+ * SpectrogramVisualization, etc.) can have different props, depending on what
+ * a particular visualization needs.
+ */
 export interface VizContainerProps {
   visualizationRecord: VisualizationRecordDetail;
   files: FilesWithContent;
