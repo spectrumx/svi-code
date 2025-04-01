d26c9ae359f673ddc6dcb8cb30aa604e6b31a62e
Connor Howington <connor.howington@gmail.com>
2025-04-01 16:30:17 -0400

    Show capture type filter in new viz wizard

--- frontend/src/pages/NewVisualizationPage.tsx
+++ frontend/src/pages/NewVisualizationPage.tsx
@@ -117,7 +117,6 @@ const NewVisualizationPage = () => {
         captures={captures}
         selectedCaptureIds={selectedCaptureId ? [selectedCaptureId] : []}
         setSelectedCaptureIds={handleCaptureSelect}
-        hideCaptureTypeFilter
       />
     </div>
   );
